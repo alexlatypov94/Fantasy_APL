113ea0846682efbfe81bc65717d59ccc56c6b060
Gurnick013 <Stivigi91@gmail.com>
2021-06-17 22:00:14 +0300

    fix: fix webpack add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -5,65 +5,64 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
-    const isProduction = options.mode === 'production';
-    const config = {
-        mode: isProduction ? 'production' : 'development',
-        entry: path.resolve(__filename, '../src/index.js'),
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            publicPath: '/',
-            filename: 'bundle.js',
+  const isProduction = options.mode === 'production';
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    entry: path.resolve(__filename, '../src/index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'build'),
+      port: 3000,
+      writeToDisk: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(woff|woff2|eot|ttf|svg|png)$/,
+          use: ['url-loader?limit=100000'],
         },
-        devServer: {
-            contentBase: path.join(__dirname, 'build'),
-            port: 3000,
-            writeToDisk: true,
-            open: true,
-            historyApiFallback: true,
-          },
-          module: {
-            rules: [
-              {
-                test: /\.(woff|woff2|eot|ttf|svg|png)$/,
-                use: ['url-loader?limit=100000'],
-              },
-              {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-              },
-              {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-              },
-              {
-                test: /\.ts(x)?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-              },
-              {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: ['source-map-loader'],
-              },
-            ],
-          },
-          plugins: [
-            new HtmlWebpackPlugin({
-              template: './src/index.html',
-            //   favicon: './src/assets/favicon.svg',
-            }),
-            new CleanWebpackPlugin(),
-            new MiniCssExtractPlugin(),
-            new CopyWebpackPlugin({
-              patterns: [{from: __dirname + '/src/assets/images',
-              to: 'assets/images',
-              noErrorOnMissing: true}],
-            }),
-          ],
-          resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-          },
-        };
-        return config;
-      };
-    
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.ts(x)?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        //   favicon: './src/assets/favicon.svg',
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: __dirname + '/src/assets/images', to: 'assets/images', noErrorOnMissing: true },
+        ],
+      }),
+    ],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx',],
+    },
+  };
+  return config;
+};
