7734edd3ec986419f425fce8c1eaaab965a19307
Aliaksei <alex.latypov94@gmail.com>
2021-07-11 14:16:39 +0300

    feat: create footer

---
+++ src/components/Footer/constants.ts
@@ -0,0 +1,10 @@
+export const DEVELOPERS = [
+  {
+    name: 'Aliaksei Latypau',
+    gitHub: 'https://github.com/alexlatypov94',
+  },
+  {
+    name: 'Nickolay Gurinovich',
+    gitHub: 'https://github.com/Gurnick013',
+  },
+];
